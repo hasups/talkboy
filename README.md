f24077710e4f4036daffdf579eab3458f39fa529
hasups <129480741+hasups@users.noreply.github.com>
2023-04-04 15:52:18 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# talkboy
-gpt-3.5-turbo python-telegram-bot on Vercel
+# chatboy
+gpt-3.5-turbo [python-telegram-bot](https://docs.python-telegram-bot.org/en/stable/) on [Vercel](https://vercel.com/dashboard)
+
+# environment variables
+- OPENAI_API_KEY
+- TELEGRAM_BOT_TOKEN
+
+# services
+- /ai
+  - [gpt-3.5-turbo](https://platform.openai.com/docs/models/gpt-3-5)
+- /image
+  - [DALL.E](https://platform.openai.com/docs/models/dall-e)
+- /tr
+  - [LibreTranslate-py](https://github.com/argosopentech/LibreTranslate-py)
+- /fc
+  - [Fortune Cookie API](http://yerkee.com/api)
+- /fact
+  - [HTTP API for useless facts](https://uselessfacts.jsph.pl/)
+- /help
+  - help
---
+++ bot.py
@@ -0,0 +1,106 @@
+#by hasups@gmail.com 2023.04.04
+
+import logging
+import os
+from flask import Flask, request
+import openai
+import telegram
+from telegram.ext import Dispatcher, MessageHandler, Filters, CommandHandler
+import json
+import requests
+from libretranslatepy import LibreTranslateAPI
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+app = Flask(__name__)
+
+# Set route /callback with POST method will trigger this method.
+@app.route('/callback', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        dispatcher.process_update(update)
+    return 'ok'
+
+
+
+bot = telegram.Bot(token=telegram_bot_token)
+lt = LibreTranslateAPI("https://translate.argosopentech.com/")
+
+def bot_chat(bot, update):
+    out = openai.ChatCompletion.create(
+        model ="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": update.message.text}],
+        max_tokens=256,
+        temperature=0.7
+        )
+    bot.send_message(chat_id=update.message.chat_id, text=out['choices'][0]['message']['content'].strip())
+
+
+def bot_help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="/ai, /image, /tr, /fact, /fc, /help")
+
+
+def ai_chat(bot, update, args):
+    prompt_in = ' '.join(args)
+    out = openai.ChatCompletion.create(
+        model ="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt_in}],
+        max_tokens=256,
+        temperature=0.7
+    )
+    bot.send_message(chat_id=update.message.chat_id, text=out['choices'][0]['message']['content'].strip())
+
+
+def ai_image(bot, update, args):
+    prompt_in = ' '.join(args)
+    message = lt.translate(prompt_in, lt.detect(prompt_in)[0]['language'], 'en')
+    out = openai.Image.create(
+      prompt = message,
+      n=1,
+      size="512x512",
+      response_format="url"
+    )
+    json_object = json.loads(str(out))
+    bot.send_photo(chat_id=update.message.chat_id, photo=json_object['data'][0]['url'], caption=message)
+
+
+def bot_trans(bot, update, args):
+    if len(args)==0:
+        bot.send_message(chat_id=update.message.chat_id, text="/tr ko|en|vi|jp|zh|... text...")
+    else:
+        prompt_in = ' '.join(args[1:])
+        message = lt.translate(prompt_in, lt.detect(prompt_in)[0]['language'], args[0])
+        bot.send_message(chat_id=update.message.chat_id, text=message)
+
+
+def fortune(bot, update):
+    out = requests.get("http://yerkee.com/api/fortune")
+    bot.send_message(chat_id=update.message.chat_id, text=out.json()['fortune'])
+
+
+def fact(bot, update):
+    out = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random", params={"language": "en"})
+    bot.send_message(chat_id=update.message.chat_id, text=out.json()['text'])
+
+
+dispatcher = Dispatcher(bot, None)
+#dispatcher.add_handler(MessageHandler(Filters.text, bot_chat))
+dispatcher.add_handler(CommandHandler('help', bot_help))
+dispatcher.add_handler(CommandHandler('ai', ai_chat, pass_args=True))
+dispatcher.add_handler(CommandHandler('image', ai_image, pass_args=True))
+dispatcher.add_handler(CommandHandler('tr', bot_trans, pass_args=True))
+dispatcher.add_handler(CommandHandler('fc', fortune))
+dispatcher.add_handler(CommandHandler('fact', fact))
+
+
+
+if __name__ == "__main__":
+    # Running server
+    app.run(debug=True)
