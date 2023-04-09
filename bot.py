55b661e48ed8a273d00b33c96410949af0dedbee
hasups <129480741+hasups@users.noreply.github.com>
2023-04-09 22:17:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,9 @@ def ai_chat(bot, update, args):
         max_tokens=256,
         temperature=0.7
     )
-    bot.send_message(chat_id=update.message.chat_id, text=out['choices'][0]['message']['content'].strip())
+    bot.send_message(chat_id=update.message.chat_id, text=prompt_in+"***")
+    update.message.reply_text(out['choices'][0]['message']['content'].strip())
+
 
 
 def ai_image(bot, update, args):
